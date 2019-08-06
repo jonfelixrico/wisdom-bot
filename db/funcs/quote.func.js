22c8519dd3c13e7eb133357aae7054dc25b528da
Felix <jonfelixrico@gmail.com>
2019-08-06 21:22:00 +0800

    Continue development on the functions

--- db/funcs/quote.func.js
+++ db/funcs/quote.func.js
@@ -1,41 +1,54 @@
 const models = require('../models'),
+    UserFuncs = require('./user.func'),
     User = models.User,
-    Quote = models.Quote;
+    Quote = models.Quote,
+    sequelize = models.sequelize;
 
+/*
+    CREATE & DELETE FUNCTIONS
+*/
 
-// if the author doesnt exist, creates the author; then creates a quote
+// Creates a quote. Tested.
 async function createQuote(authorId, content) {
-    const author = await User.findOrCreate({ where: { discordId: authorId } });
-    return await Quote.create({ content, Author: author });
+
+    // finds the user associated with the authorId
+    // if no records are found, registers the authorId and then creates it
+    let user = await User.findOrCreate({ where: { discordId: authorId } });
+    user = user[0]; // the func returns an array so we need to pluck its only result
+
+    // check if the plucked user is currently muted; throws an error if they are
+    if (await user.getActiveMuteFlag()) {
+        throw new Error(`User is currently muted.`);
+    }
+
+    // create the quote and return it
+    return await user.createQuote({ content });
 }
 
 // deletes a quote from the database
 async function deleteQuote(adminId, quoteUuid) {
-    const user = await getUser(adminId);
+    // verify if the id associated with parameter adminId is really an admin
+    await UserFuncs.findAdmin(adminId);
 
-    if (!user.adminLevel) {
-        return null;
-    }
-
+    // finds the quote associated with the given uuid
     const quote = await Quote.findOne({ where: { quoteUuid } });
 
+    // return null if no quotes match
     if (!quote) {
         return null;
     }
 
-    quote.destroy();
-    return quote;
+    // destroy the quote from the db and return the leftovers
+    return await quote.destroy();
 }
 
+/*
+    QUOTE-FETCHING FUNCTIONS
+*/
 
-async function approveQuote(approverId, quoteUuid) {
-
-}
-
-async function forceApproveQuote(adminId, quoteUuid) {
-
-}
-
+// Randomly grabs a quote from the database.
 async function getRandomQuote() {
-
+    return await Quote.findOne({ order: sequelize.literal('rand()') });
 }
+
+module.exports = { createQuote, deleteQuote, getRandomQuote };
